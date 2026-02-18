8c377dc914bccb86ba3c8f71e5287acca5db1d53
aliakseyenka <m.alexeyanko@intertechsolution.com>
2026-02-18 11:34:14 +0300

    9

--- resources/js/components/PostComponent.vue
+++ resources/js/components/PostComponent.vue
@@ -1,18 +1,34 @@
 <template>
     <div>
-<!--        <single-post-component>-->
-            <SinglePostComponent>
+        <SinglePostComponent></SinglePostComponent>
+        <div>Name: {{name}}</div>
 
-            </SinglePostComponent>
-
-<!--        </single-post-component>-->
     </div>
 </template>
 
 <script>
 import SinglePostComponent from "./SinglePostComponent.vue";
+
 export default {
     name: 'PostComponent',
+
+    data() {
+        return {
+            name: 'Vasya',
+            age: 20,
+            boolean: false,
+            sameNull: null,
+            arr: [1,
+                [
+                    3,
+                ]
+            ],
+            someObj: {
+                person: 'Ivan'
+            },
+
+        }
+    },
 
     components: {
         SinglePostComponent
