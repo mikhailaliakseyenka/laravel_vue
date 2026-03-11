a6a680bd335d3a98cdb76e18a3b2c6d96b3a73a1
aliakseyenka <m.alexeyanko@intertechsolution.com>
2026-03-11 12:47:30 +0300

    23

--- resources/js/components/IndexComponent.vue
+++ resources/js/components/IndexComponent.vue
@@ -13,15 +13,8 @@
             </thead>
             <tbody>
             <template v-for="person in people">
-                <tr :class="isEdit(person.id) ? 'd-none' : ''">
-                    <th scope="row">{{ person.id }}</th>
-                    <td>{{ person.name }}</td>
-                    <td>{{ person.age }}</td>
-                    <td>{{ person.job }}</td>
-                    <td><a href="#" @click.prevent="changeEditPersonId(person.id, person.name, person.age, person.job)" class="btn btn-success">Edit</a></td>
-                    <td><a href="#" @click.prevent="deletePerson(person.id)" class="btn btn-danger">Delete</a></td>
-                </tr>
-                    <EditComponent :person="person" :ref="`edit_${person.id}`"></EditComponent>
+                <ShowComponent :person="person" :ref="`show_${person.id}`"></ShowComponent>
+                <EditComponent :person="person" :ref="`edit_${person.id}`"></EditComponent>
             </template>
             </tbody>
         </table>
@@ -30,6 +23,8 @@
 
 <script>
 import EditComponent from "./EditComponent.vue";
+import ShowComponent from "./ShowComponent.vue";
+
 export default {
 
     data() {
@@ -49,6 +44,7 @@ export default {
 
     components: {
         EditComponent,
+        ShowComponent,
     },
 
 
